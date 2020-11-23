5b46624864e740768559f225026c1d9eaebb7840
mohammed19umer11 <mohammed19umer11@gmail.com>
2020-11-23 14:09:22 +0500

    Bla

--- Program.cs
+++ Program.cs
@@ -7,13 +7,13 @@ namespace CompilerConstruction
     {
         static void Main(string[] args)
         {
-            String code_a = "int a = 1_\n a+=2_\n if(a==2 || a<2 && a>0){}";
-            String code_b = "int a=1_";
+            String code_a = "int a = 1_\n a+=2_\n if(a==2 || a<=2 && a>0){a++_}\n int [] array_";
+            String code_b = "int a=1\na==3";
             // Console.WriteLine("Hello World!");
 
             // String [,] keywords = new String [,] {{"int","Data Type"},{"string","Data Type"},{"float","Data Type"},
             // {"double","Data Type"},{"bool","Data Type"}}; 
-            foreach(Token token in tokenize(code_a)){
+            foreach(Token token in tokenize(code_b)){
                 Console.WriteLine("TOKEN => Line No : {0}   Word = {1}",token.Line,token.Word);
             }
             
@@ -38,25 +38,25 @@ namespace CompilerConstruction
             for(int index=0;index<code.Length-1;index+=(jump)?2:1){
                 jump=false;
                 // "+ and -"
-                if(Array.Exists(operator_1, element => code[index].ToString() == element)){
+                if(Array.Exists(operator_1, element => element == code[index].ToString())){
                     if(!String.IsNullOrEmpty(temp)){
                         words.Add(new Token(temp,line)); 
                         temp="";
                         temp+=code[index];
                     if(!(index+1 == code.Length-1)){
-                        if(Array.Exists(operator_3, element => code[index+1].ToString() == element)){
+                        if(Array.Exists(operator_3, element => element == code[index+1].ToString())){
                             temp+=code[index+1];
                             words.Add(new Token(temp,line));
                             temp="";
                             jump=true;
                         }
-                        else if(code[index+1].ToString()=="+"){
+                        else if(code[index+1].Equals("+")){
                             temp+=code[index+1];
                             words.Add(new Token(temp,line));
                             temp="";
                             jump=true;
                         }
-                        else if(code[index+1].ToString()=="-"){
+                        else if(code[index+1].Equals("-")){
                             temp+=code[index+1];
                             words.Add(new Token(temp,line));
                             temp="";
@@ -67,56 +67,56 @@ namespace CompilerConstruction
                             temp="";
                         }
                     }
-                    // else{
-                    //     words.Add(new Token(temp,line));
-                    //     temp="";
-                    // } 
+                    else{
+                        words.Add(new Token(temp,line));
+                        temp="";
+                    } 
                 }
                 }
                 // "/ and *"
-                else if(Array.Exists(operator_2, element => code[index].ToString() == element)){
-                    if(!String.IsNullOrEmpty(temp)){
-                    words.Add(new Token(temp,line));
-                    temp="";
-                    temp+=code[index];
-                    if(!(index+1 == code.Length-1)){
-                        if(Array.Exists(operator_3, element => code[index+1].ToString() == element)){
-                            temp+=code[index+1];
-                            words.Add(new Token(temp,line));
-                            temp="";
-                            jump=true;
-                        }
-                        else if(code[index+1].ToString()=="/"){
-                            temp+=code[index+1];
-                            words.Add(new Token(temp,line));
-                            temp="";
-                            jump=true;
-                        }
-                        else if(code[index+1].ToString()=="*"){
-                            temp+=code[index+1];
-                            words.Add(new Token(temp,line));
-                            temp="";
-                            jump=true;
-                        }
-                        else{
-                            words.Add(new Token(temp,line));
-                            temp="";
-                        }
-                    }
-                    // else{
-                    //     words.Add(new Token(temp,line));
-                    //     temp="";
-                    // }
-                }
-                }
-                // "="
-                else if(Array.Exists(operator_3, element => code[index].ToString() == element)){
+                else if(Array.Exists(operator_2, element => element == code[index].ToString())){
                     if(!String.IsNullOrEmpty(temp)){
                         words.Add(new Token(temp,line));
                         temp="";
                         temp+=code[index];
                     if(!(index+1 == code.Length-1)){
-                        if(Array.Exists(operator_3, element => code[index+1].ToString() == element)){
+                        if(Array.Exists(operator_3, element => element == code[index+1].ToString())){
+                            temp+=code[index+1];
+                            words.Add(new Token(temp,line));
+                            temp="";
+                            jump=true;
+                        }
+                        else if(code[index+1].Equals("/")){
+                            temp+=code[index+1];
+                            words.Add(new Token(temp,line));
+                            temp="";
+                            jump=true;
+                        }
+                        else if(code[index+1].Equals("*")){
+                            temp+=code[index+1];
+                            words.Add(new Token(temp,line));
+                            temp="";
+                            jump=true;
+                        }
+                        else{
+                            words.Add(new Token(temp,line));
+                            temp="";
+                        }
+                    }
+                    else{
+                        words.Add(new Token(temp,line));
+                        temp="";
+                    }
+                }
+                }
+                // "="
+                else if(Array.Exists(operator_3, element => element==code[index].ToString())){
+                    if(!String.IsNullOrEmpty(temp)){
+                        words.Add(new Token(temp,line));
+                        temp="";
+                        temp+=code[index];
+                    if(!(index+1 == code.Length-1)){
+                        if(Array.Exists(operator_3, element => element==code[index+1].ToString())){
                             temp+=code[index+1];
                             words.Add(new Token(temp,line));
                             temp="";
@@ -127,66 +127,71 @@ namespace CompilerConstruction
                             temp="";
                         }
                     } 
-                    // else{
-                    //     words.Add(new Token(temp,line));
-                    //     temp="";
-                    // }
+                    else{
+                        temp+=code[index];
+                        words.Add(new Token(temp,line));
+                        temp="";
+                    }
                 }
                 }
                 // "< and >"
-                else if(Array.Exists(operator_4, element => code[index].ToString() == element)){
+                else if(Array.Exists(operator_4, element => element == code[index].ToString())){
                     if(!String.IsNullOrEmpty(temp)){
                         words.Add(new Token(temp,line));
                         temp="";
                         temp+=code[index];
                     if(!(index+1 == code.Length-1)){
-                        if(Array.Exists(operator_3, element => code[index+1].ToString() == element)){
+                        if(Array.Exists(operator_3, element => element == code[index+1].ToString())){
                             temp+=code[index+1];
                             words.Add(new Token(temp,line));
                             temp="";
                             jump=true;
                         }
-                        else{
+                        else {
                             words.Add(new Token(temp,line));
                             temp="";
                         }
                     }
-                    // else{
-                    //     words.Add(new Token(temp,line));
-                    //     temp="";
-                    // }
+                    else{
+                        words.Add(new Token(temp,line));
+                        temp="";
+                    }
                 }
                 }
                 // |
-                else if(Array.Exists(operator_5, element => code[index].ToString() == element)){
+                else if(Array.Exists(operator_5, element => element == code[index].ToString())){
                     if(!String.IsNullOrEmpty(temp)){
                         words.Add(new Token(temp,line));
                         temp="";
                         temp+=code[index];
                     if(!(index+1 == code.Length-1)){
-                        if(Array.Exists(operator_5, element => code[index+1].ToString() == element)){
+                        if(Array.Exists(operator_5, element => element == code[index+1].ToString())){
                             temp+=code[index+1];
                             words.Add(new Token(temp,line));
                             temp="";
                             jump=true;
                         }
                         else{
-                        // Return Error with line No 
+                        // Return Error with line No
+                            words.Add(new Token(temp,line));
+                            temp=""; 
                         }
                     }
                     else{
                         // Return Error with line No
+                        words.Add(new Token(temp,line));
+                        temp="";
                     }
                 }
                 }
                 // "&"
-                else if(Array.Exists(operator_6, element => code[index].ToString() == element)){
+                else if(Array.Exists(operator_6, element => element == code[index].ToString())){
                     if(!String.IsNullOrEmpty(temp)){
                         words.Add(new Token(temp,line));
                         temp="";
                         temp+=code[index];
                     if(!(index+1 == code.Length-1)){
-                        if(Array.Exists(operator_6, element => code[index+1].ToString() == element)){
+                        if(Array.Exists(operator_6, element => element == code[index].ToString())){
                             temp+=code[index+1];
                             words.Add(new Token(temp,line));
                             temp="";
@@ -194,21 +199,25 @@ namespace CompilerConstruction
                         }
                         else{
                         // Return Error with line No 
+                            words.Add(new Token(temp,line));
+                            temp="";
                         }
                     }
                     else{
                         // Return Error with line No 
+                        words.Add(new Token(temp,line));
+                        temp="";
                     }
                 }
                 }
                 // "!"
 
                 // Punctuators
-                else if(Array.Exists(punctuators, element => code[index].ToString() == element)){
+                else if(Array.Exists(punctuators, element => element == code[index].ToString())){
                     if(!String.IsNullOrEmpty(temp)){
                         words.Add(new Token(temp,line));
                         temp="";
-                        if(!(code[index].ToString() == " ")){
+                        if(!(code[index].ToString()==(" "))){
                             temp+=code[index];
                             words.Add(new Token(temp,line));
                             temp="";
@@ -220,7 +229,7 @@ namespace CompilerConstruction
                     // }
                     
                 }
-                else if(code[index].ToString() == "_"){
+                else if(code[index].ToString()==("_")){
                    if(!String.IsNullOrEmpty(temp)){
                         words.Add(new Token(temp,line));
                         temp="";
@@ -233,7 +242,11 @@ namespace CompilerConstruction
                     //     temp="";
                     // } 
                 }
-                else if(code[index].ToString() == "\n"){
+                else if(code[index].ToString()==("\n")){
+                    if(!String.IsNullOrEmpty(temp)){
+                        words.Add(new Token(temp,line));
+                        temp="";
+                    }
                     line++;
                 }
                 else {
@@ -244,3 +257,4 @@ namespace CompilerConstruction
         }
     }
 }
+
